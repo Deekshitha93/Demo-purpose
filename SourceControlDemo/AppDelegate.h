edb8e27e5e5035ea7342473688d213c4130b74e5
Deekshitha <deekshitha@robosoftin.com>
2016-11-09 09:06:33 +0530

    Initial Commit

---
+++ SourceControlDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SourceControlDemo
+//
+//  Created by Naveen krishnan on 09/11/16.
+//  Copyright © 2016 Robosoft Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
